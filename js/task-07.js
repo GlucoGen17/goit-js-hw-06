6bedb9300e74d0c99314805fd92aeabb2355b6dc
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-08 23:01:56 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -3,3 +3,21 @@
 // <input id="font-size-control" type="range" min="16" max="96" />
 // <br />
 // <span id="text">Abracadabra!</span>
+
+// const input = document.querySelector("#font-size-control");
+// const text = document.querySelector('#text');
+
+// // input.addEventListener('change', FontSize);
+
+// // function FontSize(event) {
+// //     text.style.fontSize = `${event.currentTarget.value}px`;
+// // }
+
+// input.addEventListener('change', event =>
+//     text.style.fontSize = `${event.currentTarget.value}px`
+// )
+
+
+document.querySelector("#font-size-control").addEventListener('change', event =>
+document.querySelector('#text').style.fontSize = `${event.currentTarget.value}px`
+)
